f3f8779e2b8b603b49e1aa64722da92f4bb16f03
Soares <soares_123@msn.com>
2024-09-22 18:51:29 +0300

    Fix import

--- business_card/app/src/main/java/com/soarespt/businesscard/MainActivity.kt
+++ business_card/app/src/main/java/com/soarespt/businesscard/MainActivity.kt
@@ -29,9 +29,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.core.graphics.toColorInt
-import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Icon
 
 
 class MainActivity : ComponentActivity() {
